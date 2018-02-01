319a9117379bb6cff071a93f0b6df9b01f8d0146
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-01-31 20:32:19 -0800

    solves four_fours

---
+++ module_0/four_fours.py
@@ -0,0 +1,12 @@
+# Create each numeric value from 0 to 9 using only four fours.
+
+print "Zero is", 4 + 4 - 4 - 4
+print "One is", (4 * 4) / (4 * 4)
+print "Two is", (4 / 4) + (4 / 4)
+print "Three is", (4 * 4 - 4) / 4
+print "Four is", 4 ** (4 - 4) * 4
+print "Five is", (4 * 4 + 4) / 4
+print "Six is", (4 + 4) / 4 + 4
+print "Seven is", 4 + 4 - (4 / 4)
+print "Eight is", 4 + 4 - 4 + 4
+print "Nine is", (4 / 4) + 4 + 4
